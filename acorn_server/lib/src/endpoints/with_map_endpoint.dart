239076eb5eec0c7077ed66f3b54a0404febc7cbc
laporte727 <ecole.la.porte@gmail.com>
2024-02-26 16:29:06 +0900

    checkout@v3

--- acorn_server/lib/src/endpoints/with_map_endpoint.dart
+++ acorn_server/lib/src/endpoints/with_map_endpoint.dart
@@ -2,17 +2,32 @@ import 'package:serverpod/serverpod.dart';
 import 'package:acorn_server/src/generated/protocol.dart';
 
 class WithMapEndpoint extends Endpoint {
-  ///Fetches WithMap from DB
+  /// Fetches WithMap from DB
   Future<List<WithMap>> getWithMap(Session session,
-      {int? keynumber}) async {
+      {List<int>? keyNumbers}) async {
+    // 条件の構築
+    var whereClauseWithMap;
+    if (keyNumbers != null && keyNumbers.isNotEmpty) {
+      for (var keyNumber in keyNumbers) {
+        if (whereClauseWithMap == null) {
+          whereClauseWithMap = WithMap.t.principalId.equals(keyNumber);
+        } else {
+          whereClauseWithMap =
+              whereClauseWithMap | WithMap.t.principalId.equals(keyNumber);
+        }
+      }
+    } else {
+      whereClauseWithMap = Constant.bool(true);
+    }
+
     return await WithMap.db.find(
       session,
-      where: (t) => keynumber !=null ? t.principalId.equals(keynumber) : Constant.bool(true),
+      where: (_) => whereClauseWithMap,
       orderBy: (withMap) => withMap.logarithm,
     );
   }
 
-  //Add a WithMap in DB
+  ///Adds a WithMap in DB
   Future<void> addWithMap(Session session, WithMap withMap) async {
     await WithMap.db.insertRow(session, withMap);
   }
