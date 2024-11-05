2873f53dd08bc047735bcf9b44127ba79fb22c4f
laporte727 <ecole.la.porte@gmail.com>
2024-11-05 10:29:04 +0900

    flutter cleanしてBuild

--- acorn_server/web/app/flutter_bootstrap.js
+++ acorn_server/web/app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1713493012"
+    serviceWorkerVersion: "2778774904"
   }
 });
--- acorn_server/web/app/flutter_service_worker.js
+++ acorn_server/web/app/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8111549ae70638b6659610b2e5b231b6",
+const RESOURCES = {"flutter_bootstrap.js": "2249dabd06e8e8f1be2b60c885bae0bf",
 "version.json": "341d03431a25888217393d0989d05c14",
-"index.html": "5507b5151cc573e9f1ef90ee372ec434",
-"/": "5507b5151cc573e9f1ef90ee372ec434",
+"index.html": "524c7f07627a91af9101167a73108aad",
+"/": "524c7f07627a91af9101167a73108aad",
 "main.dart.js": "daadfd1706452cebddad270d7051b63c",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "b15617bdc5e1d2660c23fcadc8cc6136",
