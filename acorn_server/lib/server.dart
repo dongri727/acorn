0a6437155b0c0aed21bee9a343d9558a1a3e3699
laporte727 <ecole.la.porte@gmail.com>
2025-12-23 19:29:23 +0900

    3.1.1対応

--- acorn_server/lib/server.dart
+++ acorn_server/lib/server.dart
@@ -1,12 +1,10 @@
-import 'package:mailer/mailer.dart';
-import 'package:mailer/smtp_server.dart';
+import 'dart:io';
+import 'package:mailer/mailer.dart' as mailer;
+import 'package:mailer/smtp_server/gmail.dart' as gmail_smpt;
 import 'package:serverpod/serverpod.dart';
-
 import 'package:acorn_server/src/web/routes/root.dart';
-
 import 'src/generated/protocol.dart';
 import 'src/generated/endpoints.dart';
-
 import 'package:serverpod_auth_server/module.dart' as auth;
 
 // This is the starting point of your Serverpod server. In most cases, you will
@@ -35,29 +33,29 @@ void run(List<String> args) async {
 
   // Serve all files in the /static directory.
   pod.webServer.addRoute(
-    RouteStaticDirectory(serverDirectory: 'app', basePath: '/'),
+    StaticRoute.directory(Directory('app')),
     '/*',
   );
 
   auth.AuthConfig.set(auth.AuthConfig(
     sendValidationEmail: (session, email, validationCode) async {
       // Retrieve the credentials
-      final gmailEmail = session.serverpod.getPassword('gmailEmail')!;
-      final gmailPassword = session.serverpod.getPassword('gmailPassword')!;
+      final gmailEmail = pod.getPassword('gmailEmail')!;
+      final gmailPassword = pod.getPassword('gmailPassword')!;
 
       //Create a SMTP client for Gmail.
-      final smtpServer = gmail(gmailEmail, gmailPassword);
+      final smtpServer = gmail_smpt.gmail(gmailEmail, gmailPassword);
 
       //Create an email message with the validation code.
-      final message = Message()
-        ..from = Address(gmailEmail)
+      final message = mailer.Message()
+        ..from = mailer.Address(gmailEmail)
         ..recipients.add(email)
         ..subject = 'Verfication code for Serverpod'
         ..html = 'Your verification code is: $validationCode';
 
       //Send the email message.
       try {
-        await send(message, smtpServer);
+        await mailer.send(message, smtpServer);
       } catch (_) {
         //Return false if the email could not be sent.
         return false;
@@ -68,22 +66,22 @@ void run(List<String> args) async {
     },
     sendPasswordResetEmail: (session, userInfo, validationCode) async {
       // Retrieve the credentials
-      final gmailEmail = session.serverpod.getPassword('gmailEmail')!;
-      final gmailPassword = session.serverpod.getPassword('gmailPassword')!;
+      final gmailEmail = pod.getPassword('gmailEmail')!;
+      final gmailPassword = pod.getPassword('gmailPassword')!;
 
       // Create a SMTP client for Gmail.
-      final smtpServer = gmail(gmailEmail, gmailPassword);
+      final smtpServer = gmail_smpt.gmail(gmailEmail, gmailPassword);
 
       // Create an email message with the password reset link.
-      final message = Message()
-        ..from = Address(gmailEmail)
+      final message = mailer.Message()
+        ..from = mailer.Address(gmailEmail)
         ..recipients.add(userInfo.email!)
         ..subject = 'Password reset link for Serverpod'
         ..html = 'Here is your password reset code: $validationCode';
 
       // Send the email message.
       try {
-        await send(message, smtpServer);
+        await mailer.send(message, smtpServer);
       } catch (_) {
         // Return false if the email could not be sent.
         return false;
